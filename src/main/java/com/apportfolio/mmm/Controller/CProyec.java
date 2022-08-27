62a8b8ee5f6d9ec2a7a79ea7cb521696d4b78a06
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-08-27 18:11:05 -0300

    modifique carpeta proyectos

--- src/main/java/com/apportfolio/mmm/Controller/CProyec.java
+++ src/main/java/com/apportfolio/mmm/Controller/CProyec.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +26,7 @@ import java.util.List;
 @RequestMapping("proyec")
 @CrossOrigin(origins = "http://localhost:4200")
 public class CProyec {
+
     @Autowired
     SProyec sProyec;
 
@@ -35,33 +36,37 @@ public class CProyec {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
-    @GetMapping("buscar/{id}")
+    @GetMapping("/buscar/{id}")
     //<NewProyec>
-    public ResponseEntity<?> buscarById(@PathVariable("id")int id){
-        if(!sProyec.existsById(id))
+    public ResponseEntity<?> buscarById(@PathVariable("id") int id) {
+        if (!sProyec.existsById(id))
             return new ResponseEntity<>(new Mensaje("No Existe el ID."), HttpStatus.BAD_REQUEST);
         NewProyec proyec = sProyec.buscarById(id).get();
         return new ResponseEntity<>(proyec, HttpStatus.OK);
     }
-    @PreAuthorize("hasRole('ADMIN')")
+    
+
+    //@PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
-    public ResponseEntity<?> create(@RequestBody dtoProyec dtopro){
-        if(sProyec.existsByTitulo(dtopro.getTitulo())){
+    public ResponseEntity<?> create(@RequestBody dtoProyec dtopro) {
+        if (sProyec.existsByTitulo(dtopro.getTitulo())) {
             return new ResponseEntity<>(new Mensaje("El Proyecto ya existe."), HttpStatus.BAD_REQUEST);
         }
-        if(StringUtils.isBlank(dtopro.getTitulo())){
+        if (StringUtils.isBlank(dtopro.getTitulo())) {
             return new ResponseEntity<>(new Mensaje("El nombre del proyecto es obligatorio"), HttpStatus.BAD_REQUEST);
         }
 
-        NewProyec proyec = new NewProyec(dtopro.getTitulo(),dtopro.getDescripcionP(),dtopro.getImg());
+        NewProyec proyec = new NewProyec(dtopro.getTitulo(), dtopro.getDescripcionP(), dtopro.getImg());
         sProyec.guardar(proyec);
         return new ResponseEntity<>(new Mensaje("El proyecto Fue agregado con exito"), HttpStatus.OK);
 
     }
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("update/{id}")
-    public ResponseEntity<?> upDate(@PathVariable("id")int id, @RequestBody dtoProyec dtopro){
-        if(!sProyec.existsById(id))
+    
+
+    //@PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> upDate(@PathVariable("id") int id, @RequestBody dtoProyec dtopro) {
+        if (!sProyec.existsById(id))
             return new ResponseEntity<>(new Mensaje("No existe ese ID"), HttpStatus.BAD_REQUEST);
         NewProyec proyec = sProyec.buscarById(id).get();
         proyec.setTitulo(dtopro.getTitulo());
@@ -70,12 +75,18 @@ public class CProyec {
         sProyec.guardar(proyec);
         return new ResponseEntity<>(new Mensaje("Se guardo con exito el proyecto"), HttpStatus.OK);
     }
-    @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id")int id){
-        if(!sProyec.existsById(id))
-            return new ResponseEntity<>(new Mensaje("No Existe ese ID del proyect"), HttpStatus.BAD_REQUEST);
+
+    //borrar
+    //@PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        //validaciones
+        if (!sProyec.existsById(id)) {
+            return new ResponseEntity<>(new Mensaje("No Existe ese ID del proyect"), HttpStatus.NOT_FOUND);
+            //cambio .BAD_REQUEST por .NOT_FOUND
+        }
         sProyec.delete(id);
         return new ResponseEntity<>(new Mensaje("Se borro con exito el proyecto"), HttpStatus.OK);
     }
+    
 }
