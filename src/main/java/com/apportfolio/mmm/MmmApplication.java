1e1aafa27fca55add5d4ffcda423074e126c4e05
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-09-19 14:14:14 -0300

    plobelm cors heroku

--- src/main/java/com/apportfolio/mmm/MmmApplication.java
+++ src/main/java/com/apportfolio/mmm/MmmApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class MmmApplication {
 
@@ -14,15 +16,16 @@ public class MmmApplication {
 		SpringApplication.run(MmmApplication.class, args);
 	}
 
-	@Bean
+
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			@Bean
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("*")
-						.allowedHeaders("*");
+						.allowedOrigins("https://frontendmmm.web.app/")
+						.allowedMethods("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-with", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
 			}
 		};
 
