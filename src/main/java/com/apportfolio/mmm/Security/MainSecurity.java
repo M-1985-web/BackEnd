9dcc8fe30453ca457d3e9ba6a8d30afa61780ce0
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-09-22 22:58:39 -0300

    modifique auth y mainsecurity

--- src/main/java/com/apportfolio/mmm/Security/MainSecurity.java
+++ src/main/java/com/apportfolio/mmm/Security/MainSecurity.java
@@ -3,20 +3,28 @@ package com.apportfolio.mmm.Security;
 import com.apportfolio.mmm.Security.Jwt.JwtEntryPoint;
 import com.apportfolio.mmm.Security.Jwt.JwtTokenFilter;
 import com.apportfolio.mmm.Security.Service.UserDetailsImpl;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -65,7 +73,7 @@ public class MainSecurity {
             .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .authorizeRequests()
-            .antMatchers("/auth/**").permitAll()
+            .antMatchers("/**").permitAll()
             .anyRequest().authenticated();
 
     http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
