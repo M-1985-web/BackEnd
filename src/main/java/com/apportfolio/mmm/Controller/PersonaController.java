826fcc62af251460c8aba5f504eb513cf0722e80
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-09-23 23:05:31 -0300

    deploy finalizado probando componente modifique path

--- src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
+++ src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("persona")
+@RequestMapping("/persona")
 @CrossOrigin(origins = "https://frontendmmm.web.app")
 
 public class PersonaController {
