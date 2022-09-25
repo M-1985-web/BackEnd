ae185224fc5ed57acf271308edee207ad0dc3a1f
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-09-25 18:28:16 -0300

    deploy desde heroku elimine persona interface y dto

--- src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
+++ src/main/java/com/apportfolio/mmm/Controller/PersonaController.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/persona")
-@CrossOrigin(origins = "https://frontendmmm.web.app")
+@RequestMapping("persona/")
+@CrossOrigin(origins = "https://frontendmmm.web.app/")
 
 public class PersonaController {
     @Autowired
     public IPersonaService iPersonaService;
 
-    @GetMapping("/persona/traer")
+    @GetMapping("traer")
     public List<Persona> getPersona() {
         return iPersonaService.getPersona();
     }
 
     //@PreAuthorize("hasRole('ADMIN')") //este renglón refiere a que estas acciones las puede hacer solo el administrador
-    @PostMapping("/persona/crear")
+    @PostMapping("crear")
     public String createPersona(@RequestBody Persona persona) {
         iPersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     //@PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/persona/borrar/{id}")
+    @DeleteMapping("borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         iPersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
@@ -36,7 +36,7 @@ public class PersonaController {
 
     //@PreAuthorize("hasRole('ADMIN')")
     //URL:PUERTO/personas/editar/4/nombre & apellido & img
-    @PutMapping("/persona/editar/{id}")
+    @PutMapping("editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
                                @RequestParam("apellido") String nuevoApellido,
@@ -55,7 +55,7 @@ public class PersonaController {
         return persona;
     }
 
-    @GetMapping("/persona/traer/perfil")
+    @GetMapping("traer/perfil")
     public Persona findPersona(){
         return iPersonaService.findPersona((long)1);
     }
